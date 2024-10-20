f20247615cf3ae910ddb922aa408b8eedfd604df
Marian <pidchashymaryan@gmail.com>
2024-10-20 23:27:40 +0300

    feat: kinde setup

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,13 +1,6 @@
 import type { NextPage } from 'next';
 
-import database from '@/db';
-import { users } from '@/db/schema';
-
-const HomePage: NextPage = async () => {
-  const usersA = await database.select().from(users);
-
-  console.log(usersA);
-
+const HomePage: NextPage = () => {
   return <h1 className="pl-3 pt-2 text-xl text-accent">Homepage</h1>;
 };
 
